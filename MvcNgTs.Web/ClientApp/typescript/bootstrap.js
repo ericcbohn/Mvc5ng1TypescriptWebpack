4003fa8a09882755a4b8094292a06d537303f71f
ericcbohn@gmail.com <bohn.eric@mayo.edu>
2018-04-18 18:15:56 -0500

    removed transpiled js and js.map files

--- MvcNgTs.Web/ClientApp/typescript/bootstrap.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-//import * as angular from 'angular';
-//import { PhoneApp } from './app.module';
-//angular.element(document).ready(() => {
-//    angular.bootstrap(document, [PhoneApp], { strictDi: true });
-//}); 
-//# sourceMappingURL=bootstrap.js.map
